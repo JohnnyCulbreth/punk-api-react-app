e865069c57955c2e274db997d93bb61471722c6a
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2023-01-30 10:25:31 -0600

    Updates

---
+++ precache-manifest.a6239b031739a4d434df46429d5d02b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40c714d85615812ebbed",
+    "url": "/punk-api-react-app/static/css/main.ea4b2fb8.chunk.css"
+  },
+  {
+    "revision": "40c714d85615812ebbed",
+    "url": "/punk-api-react-app/static/js/main.40c714d8.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/punk-api-react-app/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "fd65eaec54bc7636a838",
+    "url": "/punk-api-react-app/static/js/2.fd65eaec.chunk.js"
+  },
+  {
+    "revision": "db811b9dbdeae88e7252",
+    "url": "/punk-api-react-app/static/js/runtime~main.db811b9d.js"
+  },
+  {
+    "revision": "9e1f6cf69f3a13ee6abb1555b9965759",
+    "url": "/punk-api-react-app/index.html"
+  }
+];
